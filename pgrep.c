59a9309f4b330d5e75c305eca4c38306810f1ad5
Andrej Nano <nano@andrejnano.sk>
2017-10-11 01:29:56 +0200

    Probably working ??!!!
    :)

--- pgrep.c
+++ pgrep.c
@@ -4,7 +4,13 @@
  *    2016/17
  *    xmarko15
  *    xnanoa00
+ *
+ *      @TODO osetrit chyby
+ *      @TODO vycistit kod
+ *      @TODO otestovat
  */
+
+
 
 // C
 #include <stdio.h>
@@ -24,27 +30,25 @@
 *******************************/
 
 std::vector<std::mutex *> locks;    // vector of mutexes
-std::mutex writelock;
+std::mutex writelock;               // atomic writing lock
 
 int line_score;                     // current score of a line
-char* line;
-int num_threads_finished = 0;
-
-
+char* line;                         // current line pointer
+int num_threads_finished = 0;       // number of finished threads
+int finished = 0;                   // not finished
 
 /*******************************
     DECLARATIONS
 *******************************/
 
 // reads line from input returns c-string
-char* read_line(int* end);
+char* read_line(int* res);
 
 // callback function for thread regex check
 void f(int ID, char* regex_string, int regex_score);
 
 // error printout and return
 int print_err(void);
-
 
 
 /*******************************
@@ -54,9 +58,11 @@ int print_err(void);
 int main(int argc, char *argv[])
 {
 
-    unsigned int min_score; // min score for printing a line
+    // min score for printing a line
+    unsigned int min_score;
 
-    typedef struct RegexPair // argument pairs structure
+    // argument pairs structure
+    typedef struct RegexPair
     {
         char *regex_string;
         int regex_score;
@@ -80,9 +86,8 @@ int main(int argc, char *argv[])
     // explicit scope (because of i & count)
     {
         int i, count;
-
         // fill pairs struct with data from program arguments
-        for (i = 3, count = 0 ; i < argc ; i += 2,count++)
+        for (i = 3, count = 0 ; i < argc ; i += 2, count++)
         {
             pairs[count].regex_string = argv[i - 1];
 
@@ -91,9 +96,8 @@ int main(int argc, char *argv[])
         }
     }
 
-
     /****************
-        LOCKS
+        LOCKS SETUP
     *****************/
 
     int num_locks = pairs_count;
@@ -101,48 +105,40 @@ int main(int argc, char *argv[])
     // resize locks vector
     locks.resize(num_locks);
 
-    // create new locks
     for (int i = 0; i < num_locks; i++)
     {
         std::mutex *new_lock = new std::mutex();        // new lock pointer
         locks[i] = new_lock;                            // move pointer to locks vector
-        (*(locks[i])).lock();     /* Pokud je treba, tak vytvoreny zamek muzeme rovnou zamknout */
+        (*(locks[i])).lock();
     }
 
-
     /****************
-        THREADS
+        THREADS SETUP
     *****************/
 
-    int num_threads = pairs_count; // == number of regex pairs
+    int num_threads = pairs_count;
 
-    std::vector<std::thread *> threads; /* pole threadu promenne velikosti */
+    std::vector<std::thread *> threads;
     threads.resize(num_threads);
 
-    // create new threads
     for (int i = 0; i < num_threads; i++)
     {
         std::thread *new_thread = new std::thread(f, i, pairs[i].regex_string, pairs[i].regex_score);
         threads[i] = new_thread;
     }
 
+
     /**********************************
-	 * LINE READING
-	 * ********************************/
+       MAGIC
+    *********************************/
 
-    int end;
+    int res = 1;
     line_score = 0;
-    line = read_line(&end);
+    line = read_line(&res);
 
-    printf("end is %d \n", end);
-
-    while (! end) // while there is something to read
+    while (res) // while there is something to read
     {
-        // printf("num_threads_finished = %d\n", num_threads_finished);
-        // printf("line_score = %d\n", line_score);
-        // printf("line_score = %d\n", min_score);
 
-        // unlock lock for each thread
         for (int i=0; i < num_threads; i++)
             locks[i]->unlock();
 
@@ -151,18 +147,26 @@ int main(int argc, char *argv[])
 
         if (line_score >= min_score)
             printf("%s\n", line);
-        printf("been here\n");
-        line_score = 0; // reset back to 0
-        free(line);
-        line = read_line(&end);
+
+        writelock.lock();
+
+            line_score = 0;
+            free(line);
+            line = read_line(&res);
+            num_threads_finished = 0;
+
+        writelock.unlock();
     }
 
+    finished = 1;
+    for (int i=0; i < num_threads; i++)
+        locks[i]->unlock();
 
     /**********************************
-	   CLEANING & JOINING
-	*********************************/
+       CLEANING & JOINING
+    *********************************/
+    free(line);
 
-    // join all threads and delete their memory
     for (int i = 0; i < num_threads; i++)
     {
         (*(threads[i])).join();
@@ -173,59 +177,48 @@ int main(int argc, char *argv[])
     {
         delete locks[i];
     }
-
-
-    printf("everything finished\n");
     return 0;
 }
 
 
-
 void f(int ID, char* regex_string, int regex_score)
 {
+    while(! finished)
+    {
+        locks[ID]->lock();
 
-        while(! locks[ID]->try_lock())
-        {
-            usleep(1);
-        }
-
-        printf("UThread %i started with regex : %s w/ score of : %i\n", ID, regex_string, regex_score);
+        if(finished)
+            break;
 
         std::regex re(regex_string);
 
         if( std::regex_match(line, re) )
         {
             writelock.lock();
-            line_score += regex_score;   // atomic writing to global variable
+                line_score += regex_score;   // atomic writing to global variable
             writelock.unlock();
         }
 
-        ++num_threads_finished;
-        printf("thread %i | num_threads_finished %i | line_score %i\n", ID, num_threads_finished, line_score);
-        locks[ID]->unlock(); // TODO
-
-    printf("Thread %i finished... \n", ID);
+        writelock.lock();
+            ++num_threads_finished; // atomic writing to global variable
+        writelock.unlock();
+    }
 }
 
 
 
-char* read_line(int* end)
-{
-    std::string line;
-    char *str;
-
-    if (std::getline(std::cin, line))
-    {
-        str = (char *)malloc(sizeof(char) * (line.length() + 1));
-        strcpy(str, line.c_str());  //convert c++ string to c-string and copy to str char*
-        *end = 0;
-        return str;
-    }
-    else
-    {
-        *end = 1;
-        return NULL;
-    }
+char *read_line(int *res) {
+	std::string l;
+	char *str;
+	if (std::getline(std::cin, l)) {
+		str=(char *) malloc(sizeof(char)*(l.length()+1));
+		strcpy(str,l.c_str());
+		*res=1;
+		return str;
+	} else {
+		*res=0;
+		return NULL;
+	}
 }
 
 int print_err(void)
@@ -233,8 +226,3 @@ int print_err(void)
     fprintf(stderr, "USAGE: pgrep MIN_SCORE RE1 SC1 [ RE2 SC2 ] [ RE3 SC3 ] ...\n");
     return 1;
 }
-
-
-
-
-//
